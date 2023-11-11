806f93e3ab4cf78fd64f0800c1ced5370e0b51f8
Julian Ellis <45700090+JulianPEllis@users.noreply.github.com>
2023-11-10 19:37:18 -0500

    Add files via upload

---
+++ dino.js
@@ -0,0 +1,46 @@
+// Chrome Dino Runner Hack
+// By MashyDev
+// Inject code into web console and start game.
+
+countCS=0
+countSpeedCS=0
+loopActive=false
+
+Runner.prototype.gameOver=function funcNameCustomGO(){
+    countCS+=1;
+    if (loopActive) { // Loop
+        if(countCS%10==0){
+            countSpeedCS += 1;
+            Runner.prototype.setSpeed=function setSpeed(opt_speed=999999){speed=opt_speed;} 
+            console.log(`Reset Speed to 999999 (ct:${countCS}) (spc:${countSpeedCS})`);
+        } // Change Speed to '999999' every 10 would-be-deaths
+        console.log(`Would be deaths: ${countCS} (spc:${countSpeedCS} every 10)`);
+    } // Disable GameOver Function
+    else if (!loopActive) {
+        return 0;
+    }
+}
+// Opt.
+// Type 'stats()' to see stats, 'startCSLoop()' to show console spam., 'stopCSLoop()' to stop console spam.
+function stats(){ 
+    console.log(`${countCS} would-be deaths.`)
+    console.log(`Runner.prototype.setSpeed function was accessed ${countSpeedCS} times.`)
+}
+
+function stopCSLoop(){
+    if (loopActive){
+        loopActive=false; // Stop loop
+    }
+    else if (!loopActive){
+        console.log('Loop is not active. Nothing changed.')
+    }
+}
+
+function startCSLoop(){
+    if (!loopActive){
+        loopActive=true; // (Re)start loop
+    }
+    else if (loopActive){
+        console.log('Loop is already active. Nothing changed.')
+    }
+}
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Dino Runner Ultimate Hack
+This hack will make it impossible for you to get a game over, and will clock deaths that should've happened, as well as keeping your high speed later in the game.
+
+This hack works on both the offline page and https://chromedino.com
+
+## How to use
+![dino-gif](https://github.com/JulianPEllis/dino-runner-hack/assets/45700090/38743a72-1ec9-4810-9447-8fa483d057e0)
+
+<details>
+<summary>What happens at the end?</summary>
+Nothing. The counter resets back to zero after hitting 999999.
+</details>
